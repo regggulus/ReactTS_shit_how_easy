38e18d33230479a12b0d22044739bf388c5f8f16
Egorych <mikhalevichegor@gmail.com>
2022-11-03 23:50:49 +0300

    typescript object

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
@@ -19,5 +19,13 @@ root.render(
 reportWebVitals();
 
 
-let a = 10;
-a = 12;
+let a = {
+    name: "Egor",
+    age: 10,
+    address: {
+        city: "Minsk",
+        country: "Belarus"
+    }
+};
+
+console.log(a)
