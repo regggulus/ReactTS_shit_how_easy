63ec487f96497c461f6e437de21fd26a9bc0d1ff
Egorych <mikhalevichegor@gmail.com>
2022-11-04 21:56:17 +0300

    01 - React кабзда как подробно/Arrow function and function declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 
+// Arrow function
+const App = () => {
+    return (
+        <div>
+            This is App component
+        </div>
+    );
+}
 //function declaration
-const App = function() {
-  return (
-    <div >
-This is App component
-    </div>
-  );
+const App2 = () => {
+    return (
+        <div>
+            This is App component
+        </div>
+    );
 }
 
 export default App;
