5d20c5aed17d3358d29c97fe4df27541f6e97c10
Egorych <mikhalevichegor@gmail.com>
2022-11-21 02:30:32 +0300

    05 - условный рендеринг Rating.tsx props.value

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ function App() {
 
             <Accordion titleValue = {"Menu"} collapsed={true}/>
             <Accordion titleValue = {"Users"} collapsed={false}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
 
         </div>
     );
