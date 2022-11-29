54a719eed7e2f7fcf242deb33a4770473d395fa4
Egorych <mikhalevichegor@gmail.com>
2022-11-29 22:51:40 +0300

    Rating.tsx callbacks

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 
+export type SetRatingPropsType = 0 | 1 | 2 | 3 | 4 | 5
 type RatingProsType = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
+    value: SetRatingPropsType
+    onClick: (value: SetRatingPropsType) => void
 }
 
 export function Rating(props: RatingProsType) {
     return (
         <div>
-            <Star selected={props.value > 0} />
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    value: SetRatingPropsType
+    onClick: (value: SetRatingPropsType) => void
 }
 function Star(props: StarPropsType) {
-    if(props.selected === true) {
-        return <span><b>star </b></span>
-    } else {
-        return <span>star </span>
-    }
-
+    return <span onClick={ () => {props.onClick(props.value)}}>{props.selected ? <b style={{color: 'greenyellow'}}>star </b> : 'star'}</span>
 }
