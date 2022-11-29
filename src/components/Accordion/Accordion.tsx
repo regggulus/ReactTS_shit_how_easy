60c5ed30ce40e9137e49512e960eb7bcb118640f
Egorych <mikhalevichegor@gmail.com>
2022-11-30 00:43:51 +0300

    refactor code callback

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -3,27 +3,26 @@ import React from "react";
 type AccordionPropsType = {
     titleValue: string,
     collapsed: boolean
-    onClick: (collapsed: boolean)=> void
+    onChange: ()=> void
 }
 
 export function Accordion(props: AccordionPropsType) {
 
         return <div>
-            <AccordionTitle title={props.titleValue} onClick={props.onClick} collapsed={!props.collapsed}/>
-            {props.collapsed && <AccordionBody/>}
+            <AccordionTitle title={props.titleValue} onChange={props.onChange}/>
+            {!props.collapsed && <AccordionBody/>}
         </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
-    collapsed: boolean
-    onClick: (collapsed: boolean)=>void
+    onChange: ()=>void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     return (
         <div>
-            <h2 onClick={ () => {props.onClick(props.collapsed)}}>{props.title}</h2>
+            <h2 onClick={props.onChange}>{props.title}</h2>
         </div>
     )
 }
