7ecfe7be7c36f0137e8fe023daf57a49f33c78f9
Egorych <mikhalevichegor@gmail.com>
2022-11-29 20:25:40 +0300

    UnControlledRating.tsx callbacks

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -9,26 +9,22 @@ export function UnControlledRating(props: RatingProsType) {
     const [value, setValue] = useState(0)
     return (
         <div>
-            <Star selected={value > 0}/><button onClick={ () => {setValue(1)}}>1</button>
-            <Star selected={value > 1}/><button onClick={ () => {setValue(2)}}>2</button>
-            <Star selected={value > 2}/><button onClick={ () => {setValue(3)}}>3</button>
-            <Star selected={value > 3}/><button onClick={ () => {setValue(4)}}>4</button>
-            <Star selected={value > 4}/><button onClick={ () => {setValue(5)}}>5</button>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
 }
 
 function Star(props: StarPropsType) {
-    return props.selected ? <span style={{color: 'gold'}}><b>star </b></span> : <span>star </span>
-
-    /*if (props.selected === true) {
-        return <span><b>star </b></span>
-    } else {
-        return <span>star </span>
-    }*/
+    return <span onClick={ () => {props.setValue(props.value)}}>{props.selected ? <b style={{color: 'yellow'}}>star </b> : 'star'}</span>
 
 }
