bf7fd612f4066d3ff4c02c4335cb7b9ac723c8da
Egorych <mikhalevichegor@gmail.com>
2023-05-17 20:24:27 +0300

    ​16 - test

--- src/components/unControlledAccordion/reducer.tsx
+++ src/components/unControlledAccordion/reducer.tsx
@@ -1,11 +1,17 @@
-type ActionType = {
+export type ActionType = {
     type: string
 }
 export const TOGGLE_COLLAPSED = 'TOGGLE-COLLAPSED'
-export const reducer = (state: boolean, action: ActionType) => {
+export type StateType = {
+    collapsed: boolean
+}
+export const reducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
         case TOGGLE_COLLAPSED :
-            return !state
+            return {
+                ...state,
+                collapsed: !state.collapsed
+            }
         default:
             throw new Error('Bad action type')
     }
