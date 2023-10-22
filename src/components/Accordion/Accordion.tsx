4e44d78e3335a9f83ab06029f6728a051be0396e
Egorych <mikhalevichegor@gmail.com>
2023-10-22 23:00:22 +0300

    01 - Components (part 2) AccordionBody

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -3,20 +3,28 @@ import React from "react";
 
 export function Accordion() {
 
-        return <div>
-           <AccordionTitle/>
+    return <div>
+        <AccordionTitle/>
+        <AccordionBody/>
 
-         <ul>
-             <li>1</li>
-             <li>2</li>
-             <li>3</li>
-         </ul>
-        </div>
+    </div>
 
     function AccordionTitle() {
         return (
             <div>
                 <h3>Menu</h3>
+            </div>
+        )
+    }
+
+    function AccordionBody() {
+        return (
+            <div>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
             </div>
         )
     }
