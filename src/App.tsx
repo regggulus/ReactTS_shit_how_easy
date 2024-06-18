565e8d6518594813cc88653a1699201d6e2d944f
Egorych <mikhalevichegor@gmail.com>
2024-06-19 01:12:55 +0300

    components App.tsx and Rating.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,24 @@
 import React from 'react';
 import './App.css';
-import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
-import {OnOff} from "./components/OnOff/OnOff";
-import {UnControlledAccordion} from "./components/UnControlledAccordion/UnControlledAccordion";
-import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
-
 
 function App() {
 
     return (
         <div className={'App'}>
-            <OnOff/>
-            <UnControlledAccordion title={'Menu'}/>
-            <UnControlledAccordion title={'Users'}/>
-            <UnControlledRating/>
-            <PageTitle title={'This is Component'}/>
-            <PageTitle title={'My Friends'}/>
-            <Accordion title={'Menu'} collapsed={false}/>
-            <Accordion title={'Users'} collapsed={false}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            This is component
         </div>
     );
 }
 
-
-type PageTitlePropsType = {
-    title: string
-}
-function PageTitle(props: PageTitlePropsType) {
+function Rating() {
     return (
-        <h1>{props.title}</h1>
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
     )
 }
-
 export default App;
