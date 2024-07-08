3f743b5d7b905da20d231705c55d1a7e4d7376ea
Egorych <mikhalevichegor@gmail.com>
2024-07-09 00:15:26 +0300

    ternary operator

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {setProjectAnnotations} from "@storybook/react";
+
 export type RatingType = {
     value: 0 | 1 | 2 | 3 | 4 | 5
 }
+
 export function Rating({value}: RatingType) {
     return (
         <div>
-            <Star selected={value > 0}/>
+            <Star selected={value > 2}/>
             <Star selected={value > 1}/>
             <Star selected={value > 2}/>
             <Star selected={value > 3}/>
             <Star selected={value > 4}/>
-
         </div>
     )
 }
@@ -21,9 +22,13 @@ type StarType = {
 }
 
 function Star({selected}: StarType) {
+    return selected ? <span><b>star </b></span> : <span>star </span>
+    /*if (selected) {
+        return (
+            <span><b>star </b></span>
+        )
+    } else {
+        return <span>star </span>
+    }*/
 
-    return (
-
-        <span>{selected}<b>star </b></span>
-    )
 }
