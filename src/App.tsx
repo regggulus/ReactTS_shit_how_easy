c2b3f99f8f7c60eaba00d56f45362b6d81184340
Egorych <mikhalevichegor@gmail.com>
2024-12-12 23:15:10 +0300

    new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Accordion} from "./components/accordion/Accordion";
-import {Rating} from "./components/rating/Rating";
-import {PageTitle} from "./components/titleMain/PageTitle";
-import {OnOff} from "./onOff/OnOff";
+
 
 function App() {
 
     return (
         <div className={'App'}>
-            <PageTitle title={'This is component'}/>
-            <Rating value = {0}/>
-            <Rating value = {1}/>
-            <Rating value = {2}/>
-            <Rating value = {3}/>
-            <Rating value = {4}/>
-            <Rating value = {5}/>
-            <Accordion title={'Menu'} collapsed={true}/>
-            <Accordion title={'Users'} collapsed={false}/>
-            <OnOff onOff={true}/>
+
         </div>
     );
 }
