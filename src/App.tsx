823a55043fdee51b4754d89b706dddcd43248ec8
Egorych <mikhalevichegor@gmail.com>
2024-12-13 23:34:23 +0300

    new component Rating

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ function App() {
 
     return (
         <div className={'App'}>
-<div>This is components App</div>
+            <div>This is components App</div>
+            <Rating/>
         </div>
     );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+
+    )
 }
 
 export default App;
