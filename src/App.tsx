9f3ed896a31c14cfd7afb49ef20e50fe67c4b1de
Egorych <mikhalevichegor@gmail.com>
2024-12-16 00:47:23 +0300

    Star props value controlled

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,12 @@ function App() {
     return (
         <div className={'App'}>
             <AppTitle/>
-            <Rating/>
+            <Rating value = {0}/>
+            <Rating value = {1}/>
+            <Rating value = {2}/>
+            <Rating value = {3}/>
+            <Rating value = {4}/>
+            <Rating value = {5}/>
             <Accordion/>
         </div>
     );
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,22 +1,84 @@
 import React from "react";
 
-export function Rating() {
+type RatingType = {
+    value: number
+}
+export function Rating(props: RatingType) {
+    if(props.value === 1) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected ={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+
+        )
+    }
+    if(props.value === 2) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected ={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+
+        )
+    }
+    if(props.value === 3) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected ={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+
+        )
+    }
+    if(props.value === 4) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected ={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+
+        )
+    }
+    if(props.value === 5) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected ={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        )
+    }
     return (
         <div>
-            <Star selected={true}/>
-            <Star selected ={true}/>
-            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected ={false}/>
+            <Star selected={false}/>
             <Star selected={false}/>
             <Star selected={false}/>
         </div>
-
     )
+
 }
 
-/*type SelectedType = {
+type SelectedType = {
     selected: boolean
-}*/
-function Star(props: any) {
+}
+function Star(props: SelectedType) {
     if (props.selected === true) {
         return <span><b>star </b></span>
     } else {
