86e8d80542b3612b159abf187ec90e0eff1ebc82
regggulus <regggulus@gmail.com>
2025-01-13 21:25:06 +0300

    Un controlled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Accordion} from "./components/Accordion";
-import {Rating} from "./components/Rating";
-import {PageTitle} from "./components/PageTitle";
+import {Accordion} from "./components/controlledComponents/Accordion";
+import {Rating} from "./components/controlledComponents/Rating";
+import {PageTitle} from "./components/controlledComponents/PageTitle";
 import {OnOff} from "./components/OnOff";
 
 
@@ -10,7 +10,7 @@ function App() {
 
     return (
         <div className={'App'}>
-           {/* <PageTitle title = {'This is components App'}/>
+           {/* <PageTitle title = {'This is controlled components App'}/>
             <Rating value = {0}/>
             <Rating value = {1}/>
             <Rating value = {2}/>
@@ -19,6 +19,7 @@ function App() {
             <Rating value = {5}/>
             <Accordion title = {'Users'} collapsed = {true}/>
             <Accordion title = {'Menu'} collapsed = {false}/>*/}
+            <PageTitle title={'This is Un Controlled components App'}/>
             <OnOff/>
         </div>
     );
---
+++ src/components/unControlledComponents/UnControlledAccordion.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type AccordionType = {
+    title: string
+    collapsed: boolean
+}
+export function Accordion (props: AccordionType) {
+
+        return (
+            <div>
+                <AccordionTitle title={props.title}/>
+                {!props.collapsed && <AccordionBody/>}
+            </div>
+        )
+}
+
+type AccordionTitleType = {
+    title: string
+}
+function AccordionTitle (props: AccordionTitleType) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+function AccordionBody () {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
